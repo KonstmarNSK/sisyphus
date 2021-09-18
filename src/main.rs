798502134d18b4f9efb513d46a05b7435b5f1373
konstantin <konstmar@yandex.ru>
2021-09-18 20:33:24 +0700

    added very basic ui templates

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,32 @@ use hyper::service::{make_service_fn, service_fn};
 use sailfish::TemplateOnce;
 
 async fn hello_world(_req: Request<Body>) -> Result<Response<Body>, Infallible> {
-
     let ctx = HelloTemplate {
         messages: vec![String::from("foo"), String::from("bar")],
     };
 
-    Ok(Response::new((ctx.render_once().unwrap() + "Hello, World").into()))
+    let links = vec![
+        Link {
+            href: "some-address-1",
+            text: "some-link-text-1",
+        },
+        Link {
+            href: "some-address-2",
+            text: "some-link-text-2",
+        },
+        Link {
+            href: "some-address-3",
+            text: "some-link-text-3",
+        },
+    ];
+
+    let left_sidebar = LeftSidebar { links };
+
+    let txt = TextParagraph{
+        text: "some_paragraph"
+    };
+
+    Ok(Response::new((left_sidebar.render_once().unwrap()  + &txt.render_once().unwrap() + "Hello, World").into()))
 }
 
 #[tokio::main]
@@ -35,10 +55,27 @@ async fn main() {
 }
 
 
-
-
 #[derive(TemplateOnce)]
 #[template(path = "hello.html")]
 struct HelloTemplate {
-    messages: Vec<String>
+    messages: Vec<String>,
+}
+
+#[derive(TemplateOnce)]
+#[template(path = "components/link.html")]
+struct Link<'a, 'b> {
+    href: &'a str,
+    text: &'b str,
+}
+
+#[derive(TemplateOnce)]
+#[template(path = "components/left-sidebar.html")]
+struct LeftSidebar<'a, 'b> {
+    links: Vec<Link<'a, 'b>>,
+}
+
+#[derive(TemplateOnce)]
+#[template(path = "components/text-paragraph.html")]
+struct TextParagraph<'a>{
+    text: &'a str
 }
